dd3b7f02239b6adf243cb71b03a203c828e23833
jhopkins39 <jhopkins39@gatech.edu>
2022-12-04 21:57:19 -0500

    around 500 of each class from stable diffusion.  Also, had the normal net test on those images.  Attempted to use our trained net to test those images, but it predicted them all as dog.  Will work more in the future

---
+++ src/test_pretrained_on_fake_jake.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
+
+from jake_dataset_loader import FakeCIFAR10
+
+
+CIFAR10_CLASSES_MAP = {
+    "airplane": 0,
+    "automobile": 1, 
+    "bird": 2, 
+    "cat": 3, 
+    "deer": 4, 
+    "dog": 5, 
+    "frog": 6, 
+    "horse": 7, 
+    "ship": 8, 
+    "truck": 9
+}
+
+def main():
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+    print(f'Using device: {device}')
+
+    torch.set_printoptions(linewidth=120)
+
+    transform_train = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), # Original
+        #transforms.Normalize((0.4919, 0.4827, 0.4472), (0.2470, 0.2434, 0.2616)) 
+    ])
+
+    real_dataset = torchvision.datasets.CIFAR10(root='./pretrained_models', train=False, download=True, transform=transform_train)
+    fake_dataset = FakeCIFAR10(train=False, transform=transform_train)
+
+    dataset = real_dataset
+    test_dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+    resnet56 = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar10_resnet56", pretrained=True)
+
+    our_net = resnet56.load_state_dict(torch.load("../fake_resnet56_auto_save.pth", map_location=device))
+
+    #our_net.eval()
+
+    confusion_matrix = torch.zeros((10, 10))
+
+    cnt = 0
+    iterations = len(test_dataloader)
+    for i in range(iterations):
+        img, label = next(iter(test_dataloader))
+
+        prediction = torch.argmax(resnet56(img))
+        
+        print(f"Prediction: {dataset.classes[prediction]:<10}  Label: {dataset.classes[label]:<10} >> {bool(prediction==label)}")
+        
+        if prediction==label:
+            cnt += 1
+        
+        img = torch.squeeze(img)
+        
+        #show(img)
+        
+        confusion_matrix[CIFAR10_CLASSES_MAP[dataset.classes[label]], CIFAR10_CLASSES_MAP[dataset.classes[prediction]]] += 1
+        
+        if 0:
+            img = torch.squeeze(img)
+            r_img = torch.clone(img)
+            g_img = torch.clone(img)
+            b_img = torch.clone(img)
+            
+            r_img[[1, 2], :, :] = 0
+            g_img[[0, 2], :, :] = 0
+            b_img[[0, 1], :, :] = 0
+
+            # show(r_img)
+            # show(g_img)
+            # show(b_img)
+            
+            # show(img)
+            
+            f, axarr = plt.subplots(2,2)
+            axarr[0,0].imshow(img.squeeze().permute(1, 2, 0))
+            axarr[0,1].imshow(r_img.squeeze().permute(1, 2, 0))
+            axarr[1,0].imshow(g_img.squeeze().permute(1, 2, 0))
+            axarr[1,1].imshow(b_img.squeeze().permute(1, 2, 0))
+            plt.show()
+            
+    print(f"Accuracy: {cnt/iterations*100}%")
+        
+    row_sums = confusion_matrix.sum(axis=1)
+    confusion_matrix 
+    confusion_matrix = torch.round(confusion_matrix / row_sums[:, None], decimals=4)
+        
+    print(confusion_matrix)
+        
+    print(row_sums)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/trainer_jake.py
@@ -0,0 +1,137 @@
+"""
+Class to train, evaluate and save a model
+
+main() contans an example of how to use this class
+"""
+
+import os
+import torch
+import torchvision
+import torch.optim as optim 
+from torch.utils.data import DataLoader
+from torchmetrics import ConfusionMatrix
+
+from jake_dataset_loader import FakeCIFAR10
+
+torch.set_printoptions(linewidth=120)
+
+
+class Trainer:
+    def __init__(self, model, train_loader, test_loader, optimizer, criterion, epochs):
+        self.model = model
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.epochs = epochs
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+        print(f'Using device: {self.device}')
+        
+    def train(self):
+        self.model.to(self.device)
+        self.model.train()
+
+        for epoch in range(self.epochs):  # loop over the dataset multiple times
+            running_loss = 0.0
+            for i, data in enumerate(self.train_loader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+
+                # zero the parameter gradients
+                self.optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+
+                running_loss += loss.item()
+                
+
+            print(f'Epoch: {epoch}, running loss: {running_loss}')
+            
+            os.makedirs(os.path.dirname('saved_models/fake_resnet56_auto_save.pth'), exist_ok=True)
+            torch.save(self.model.state_dict(), 'saved_models/fake_resnet56_auto_save.pth')
+            
+        print('Finished Training')
+
+    def save_model(self, path='saved_models/fake_resnet56.pth'):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        if os.path.exists(path):
+            path = path.replace('.pth', '_new.pth')
+            
+        torch.save(self.model.state_dict(), path)
+        
+    def evaluate(self):
+        self.model.eval()
+        
+        confusion_matrix = ConfusionMatrix(num_classes=10, normalize='true', task='multiclass').to(self.device)
+        
+        correct_predictions = 0
+        with torch.no_grad():
+            for data in self.test_loader:
+                images, labels = data
+                images, labels = images.to(self.device), labels.to(self.device)
+
+                print("images: ")
+                print(images)
+                
+                out = self.model(images)
+                predictions = torch.argmax(out, dim=1)
+                
+                correct_predictions += torch.sum(predictions==labels)
+                confusion_matrix.update(predictions, labels)    
+
+        accuracy = correct_predictions / len(self.test_loader.dataset)
+                    
+        return accuracy, confusion_matrix
+    
+    def run(self, save_model=True):
+        self.train()
+        
+        if save_model:
+            self.save_model()
+            
+        return self.evaluate()
+    
+    def _load_model(self, path):
+        self.model.load_state_dict(torch.load(path, map_location=self.device))
+        
+    def test_loaded_model(self, path='saved_models/fake_resnet56.pth'):
+        self._load_model(path)
+        return self.evaluate()
+
+def main():
+
+    fake_train_set = FakeCIFAR10(train=True, transform=FakeCIFAR10.standard_transform)
+    fake_test_set = FakeCIFAR10(train=False, transform=FakeCIFAR10.standard_transform)
+    
+    real_test_set = torchvision.datasets.CIFAR10(root='./pretrained_models', train=False, download=True, transform=FakeCIFAR10.standard_transform)
+    
+    train_set = fake_train_set
+    test_set = fake_test_set
+
+    train_dataloader = DataLoader(train_set, batch_size=256, shuffle=True)
+    test_dataloader = DataLoader(test_set, batch_size=256, shuffle=True)
+
+    resnet56 = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar10_resnet56", pretrained=False)
+    
+    model = resnet56
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0005, nesterov=True)
+
+    trainer = Trainer(model, train_dataloader, test_dataloader, optimizer, criterion, epochs=200)
+    
+    #accuracy, confusion_matrix = trainer.run()
+    accuracy, confusion_matrix = trainer.test_loaded_model("../fake_resnet56_auto_save.pth")
+    
+    print(f"Accuracy: {accuracy*100}%")
+    print(confusion_matrix.compute())
+
+
+if __name__ == "__main__":
+    main()
